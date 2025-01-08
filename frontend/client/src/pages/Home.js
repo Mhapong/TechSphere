3a598dd95197e8649bba30732b9a0ae1efd9fd66
6710110151 <chinochi866@gmail.com>
2025-01-08 09:22:23 +0700

    Separate admin and client file

---
+++ frontend/client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, Box, AppBar } from "@mui/material";
+import { useState } from "react";
+
+export default function Home() {
+    return (
+
+        <div>
+            <AppBar></AppBar>
+            <header>
+
+            </header>
+            <body>
+                <Button variant="contained">Hello world</Button>
+                <h1>allahhhhhhhhh</h1>
+
+            </body>
+        </div>
+    );
+};
