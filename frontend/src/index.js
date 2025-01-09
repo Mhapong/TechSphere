1d345e1fc7235fe1fa3e538c75ae6190d8870881
PunPK <phurutsakorn.kps@gmail.com>
2025-01-09 20:49:48 +0700

    Add: Navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Home from './pages/Home';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Home from "./pages/Home";
+import View_admin from "./admin/pages/view";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Home />
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Button, Box, AppBar } from "@mui/material";
 import { useState } from "react";
+import Navbar from "../components/Navbar";
+// import axios from "axios";
+
+// axios.defaults.baseURL =
+//   process.env.REACT_APP_BASE_URL || "http://localhost:1337";
 
 export default function Home() {
-    return (
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <h1>Hello</h1>
+        <Button variant="contained">Hello world</Button>
+      </div>
+    </div>
+  );
+}
 
-        <div>
-            <AppBar></AppBar>
-            <header>
+// import ButtonAppBar from "../admin/components/Navbar";
 
-            </header>
-            <body>
-                <Button variant="contained">Hello world</Button>
-                <h1>allahhhhhhhhh</h1>
-
-            </body>
-        </div>
-    );
-};
+// export default function Home() {
+//   return (
+// }
