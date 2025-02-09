7cc02ba311724f84fbacc4c045446dd0ba91848f
Pathompong Sirisawat <6710110239@psu.ac.th>
2025-02-09 15:14:33 +0700

    merge study page and my course page v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import {
+  Link,
+  Navigate,
   BrowserRouter as Router,
   Route,
   Routes,
@@ -19,108 +21,74 @@ import Mycourse from "./client/pages/MyCourse";
 import Profile from "./admin/pages/profile";
 import EditProfile from "./admin/pages/edit-profile";
 import AddCourse from "./admin/pages/CreateCourse";
-import BuyCourse from "./client/pages/buyCourse";
-import ContentStudy from "./client/pages/ContentStudy.js";
 import AddTopic from "./admin/pages/CreateTopic";
 import CourseDetails from "./admin/pages/CreateSummarize";
 import LecturerAll from "./admin/pages/Lecturer";
-import Explore from "./client/pages/explore.js";
+import StudentTable from "./admin/pages/student";
+import HomeAdmin from "./admin/pages/home-admin";
 
 function App() {
   const { state } = useContext(AuthContext);
+  console.log(state.user);
 
-  if (!state.isLoggedIn) {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginForm />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  } else {
-    const userRole = state.user?.userRole;
-    if (userRole === "Student") {
-      return (
-        <BrowserRouter>
+  const userRole = state.user?.userRole;
+
+  return (
+    <BrowserRouter>
+      {state.isLoggedIn ? (
+        userRole === "User" ? (
+          <>
+            <Nav />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/my-course" element={<Mycourse />} />
+            </Routes>
+          </>
+        ) : userRole === "Lecturer" ? (
+          <>
+            <Nav />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </>
+        ) : userRole === "Admin" ? (
+          <>
+            <NavAdmin />
+            <Routes>
+              <Route path="/" element={<HomeAdmin />} />
+              <Route path="/create-course" element={<AddCourse />} />
+              <Route
+                path="/create-topic/:courseid/:coursename"
+                element={<AddTopic />}
+              />
+              <Route
+                path="/create-summarize/:courseid"
+                element={<CourseDetails />}
+              />
+              <Route path="/view" element={<Home />} />
+              <Route path="/lecturer" element={<LecturerAll />} />
+              <Route path="/student" element={<StudentTable />} />
+              <Route path="/finance" element={<Home />} />
+              <Route path="/user" element={<Profile />} />
+              <Route path="/edit-profile/:userid" element={<EditProfile />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </>
+        ) : null
+      ) : (
+        <>
+          <Nav />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/sign-up" element={<SignUp />} />
-            <Route
-              path="/topic/:subject_title/:username/:subject"
-              element={<TopicStudent />}
-            />
-            <Route path="/user" element={<UserPage />} />
-            <Route path="/contact" element={<ContactLecturer />} />
+            <Route path="/my-course" element={<Mycourse />} />
           </Routes>
-        </BrowserRouter>
-      </>
-    );
-  else {
-    console.log(state.user.userRole);
-    const userRole = state.user?.userRole;
-    if (userRole === "User") {
-      return (
-        <>
-          <Nav />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/explore" element={<Explore />} />
-              <Route path="/my-course" element={<Mycourse />} />
-              <Route path="/contentstudy" element={<ContentStudy />} />
-              <Route
-                path="/view-product/:name/:documenId"
-                element={<BuyCourse />}
-              />
-            </Routes>
-          </BrowserRouter>
         </>
-      );
-    } else if (userRole === "Lecturer") {
-      return (
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<LoginForm />} />
-            <Route
-              path="/topic/:subject_title/:username/:subject/:documentId"
-              element={<TopicLecturer />}
-            />
-            <Route
-              path="/topic/detail/:topic_title/:subject/:max_score/:id/:documentId"
-              element={<DetailTopicLecturer />}
-            />
-            <Route path="/topic/create/:subject" element={<CreateTopic />} />
-            <Route
-              path="/topic/addscore/:subject"
-              element={<AddScoreTopic />}
-            />
-            <Route
-              path="/subject/addstudent/:id/:subject"
-              element={<AddStudent />}
-            />
-            <Route
-              path="/subject/student-all/:id/:subject"
-              element={<StudentListPage />}
-            />
-            <Route
-              path="/subject/student/:id/:subject"
-              element={<AddIDStudent />}
-            />
-            <Route
-              path="/subject/edit/:subject"
-              element={<EditSubjectInfo />}
-            />
-            <Route path="/subject/create" element={<CreateSubject />} />
-            <Route path="/" element={<HomeLecturer />} />
-            <Route path="/user" element={<UserPage />} />
-            <Route path="/excelfetch" element={<ExcelFetch />} />
-          </Routes>
-        </BrowserRouter>
-      );
-    }
-  }
+      )}
+    </BrowserRouter>
+  );
 }
 
 export default App;
