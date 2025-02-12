6b012258d2c986a35f411fc4d2eccc40ece8264e
PunPK <phurutsakorn.kps@gmail.com>
2025-02-12 21:25:49 +0700

    Add: SearchBar in ALLAdminPage

--- frontend/src/admin/pages/Course.js
+++ frontend/src/admin/pages/Course.js
@@ -10,13 +10,9 @@ import {
 } from "@material-tailwind/react";
 import React, { useEffect, useState } from "react";
 import ax from "../../conf/ax";
-import ReactMarkdown from "react-markdown";
-import usericon from "../components/Image/usertest.png";
+// import ReactMarkdown from "react-markdown";
 import { useNavigate } from "react-router";
-import { Edit, Add } from "@mui/icons-material";
-import { motion } from "framer-motion";
-import { Image } from "@mui/icons-material";
-import remarkGfm from "remark-gfm";
+// import remarkGfm from "remark-gfm";
 import CourseEx from "../components/Image/htmlcssJS.webp";
 // import datapic from "../components/data.png";
 // import webpic from "../components/web-100.png";
@@ -29,7 +25,13 @@ import CourseEx from "../components/Image/htmlcssJS.webp";
 export function CourseView() {
   const [Course, setCourse] = useState([]);
   const Navigate = useNavigate();
+  const [queryCourse, setQueryCourse] = useState("");
 
+  const filteredCourse = queryCourse
+    ? Course.filter((value) =>
+        value.Name.toLowerCase().includes(queryCourse.toLowerCase())
+      )
+    : Course;
   useEffect(() => {
     // Fetch team values from Strapi
     const fetchCourse = async () => {
@@ -58,20 +60,36 @@ export function CourseView() {
         <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
           คอร์สเรียนทั้งหมดที่มีใน TechSphere ทั้งหมด {Course.length} คอร์ส
         </p>
+        <div className="flex justify-center items-center space-x-3 bg-white p-4 rounded-lg shadow-md border border-gray-200">
+          <label htmlFor="search" className="text-gray-700 text-lg font-medium">
+            ค้นหา :
+          </label>
+          <input
+            id="search"
+            title="ค้นหา"
+            type="text"
+            placeholder="ค้นหาโดยชื่อคอร์สเรียน"
+            className="flex-1 bg-gray-100 focus:bg-white h-10 w-72 border border-gray-300 rounded-lg px-4 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
+            value={queryCourse}
+            onChange={(e) => setQueryCourse(e.target.value)}
+          />
+        </div>
       </div>
       {/* <div className="relative   items-center">
           <div
             id="slider"
             className=" h-full w-full my-7 grid grid-cols-3 gap-2 items-center place-content-center scrollbar-hide"
           > */}
-      <div className="h-full w-full my-7 grid grid-cols-3 gap-10 items-stretch place-content-center scrollbar-hide">
-        {Course.map((items) => (
-          <div key={items.id} className="h-full">
-            <Card className="w-auto max-w-[26rem] h-full shadow-lg flex flex-col">
-              <CardHeader floated={false} color="blue-gray">
-                <img src={CourseEx} alt="ui/ux review check" />
-                <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-                {/* <IconButton
+      <div>
+        {filteredCourse.length > 0 ? (
+          <div className="h-full w-full my-7 grid grid-cols-3 gap-10 items-stretch place-content-center scrollbar-hide">
+            {filteredCourse.map((items) => (
+              <div key={items.id} className="h-full">
+                <Card className="w-auto max-w-[26rem] h-full shadow-lg flex flex-col">
+                  <CardHeader floated={false} color="blue-gray">
+                    <img src={CourseEx} alt="ui/ux review check" />
+                    <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
+                    {/* <IconButton
                   size="sm"
                   color="red"
                   variant="text"
@@ -86,97 +104,112 @@ export function CourseView() {
                     <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
                   </svg>
                 </IconButton> */}
-              </CardHeader>
+                  </CardHeader>
 
-              <CardBody>
-                <div className="mb-3 flex items-center justify-between">
-                  <Typography
-                    variant="h5"
-                    color="blue-gray"
-                    className="font-medium"
-                  >
-                    {items.Name}
-                  </Typography>
-                  <Typography
-                    color="blue-gray"
-                    className="flex items-center gap-1.5 font-normal"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="-mt-0.5 h-5 w-5 text-yellow-700"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                    5.0
-                  </Typography>
-                </div>
-                <Typography color="gray">
-                  {" "}
-                  {items.Description && items.Description.length > 70
-                    ? `${items.Description.slice(0, 70)}...`
-                    : items.Description}
-                </Typography>
-                <div className="group mt-8 inline-flex flex-wrap items-center gap-3">
-                  {items &&
-                    items.categories.map((value) => (
-                      <div
-                        key={value.id}
-                        className="inline-flex bg-gray-200 rounded-full px-3 py-0 text-sm font-semibold text-gray-700 mr-2 mb-2 mt-0 items-center"
+                  <CardBody>
+                    <div className="mb-3 flex items-center justify-between">
+                      <Typography
+                        variant="h5"
+                        color="blue-gray"
+                        className="font-medium"
                       >
-                        {value.tag}
-                        <Tooltip content={value.tag}>
-                          <span className="cursor-pointer grid rounded-full border border-gray-900/5 bg-gray-900/5 p-3 text-gray-900 transition-colors hover:border-gray-900/10 hover:bg-gray-900/10 hover:!opacity-100 group-hover:opacity-70 ml-2">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 24 24"
-                              fill="currentColor"
-                              className="h-5 w-5"
-                            >
-                              <path d="M12 7.5a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
-                              <path
-                                fillRule="evenodd"
-                                d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 14.625v-9.75zM8.25 9.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM18.75 9a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V9.75a.75.75 0 00-.75-.75h-.008zM4.5 9.75A.75.75 0 015.25 9h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H5.25a.75.75 0 01-.75-.75V9.75z"
-                                clipRule="evenodd"
-                              />
-                              <path d="M2.25 18a.75.75 0 000 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 00-.75-.75H2.25z" />
-                            </svg>
-                          </span>
-                        </Tooltip>
-                      </div>
-                    ))}
-                </div>
-              </CardBody>
-              <CardFooter className="flex flex-col justify-between mt-auto pb-3">
-                <div className="grid grid-cols-2 gap-2">
-                  <Button
-                    onClick={() => {
-                      Navigate(`/create-summarize/${items.documentId}`);
-                    }}
-                    size="lg"
-                    className="w-full"
-                  >
-                    ดูข้อมูลคอร์ส
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      Navigate("/view-student", { state: { Value: items } });
-                    }}
-                    size="lg"
-                    className="w-full"
-                  >
-                    ดูข้อมูลนักเรียน
-                  </Button>
-                </div>
-              </CardFooter>
-            </Card>
+                        {items.Name}
+                      </Typography>
+                      <Typography
+                        color="blue-gray"
+                        className="flex items-center gap-1.5 font-normal"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="-mt-0.5 h-5 w-5 text-yellow-700"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        5.0
+                      </Typography>
+                    </div>
+                    <Typography color="gray">
+                      {" "}
+                      {items.Description && items.Description.length > 70
+                        ? `${items.Description.slice(0, 70)}...`
+                        : items.Description}
+                    </Typography>
+                    <div className="group mt-8 inline-flex flex-wrap items-center gap-3">
+                      {items &&
+                        items.categories.map((value) => (
+                          <div
+                            key={value.id}
+                            className="inline-flex bg-gray-200 rounded-full px-3 py-0 text-sm font-semibold text-gray-700 mr-2 mb-2 mt-0 items-center"
+                          >
+                            {value.tag}
+                            <Tooltip content={value.tag}>
+                              <span className="cursor-pointer grid rounded-full border border-gray-900/5 bg-gray-900/5 p-3 text-gray-900 transition-colors hover:border-gray-900/10 hover:bg-gray-900/10 hover:!opacity-100 group-hover:opacity-70 ml-2">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 24 24"
+                                  fill="currentColor"
+                                  className="h-5 w-5"
+                                >
+                                  <path d="M12 7.5a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 14.625v-9.75zM8.25 9.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM18.75 9a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V9.75a.75.75 0 00-.75-.75h-.008zM4.5 9.75A.75.75 0 015.25 9h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H5.25a.75.75 0 01-.75-.75V9.75z"
+                                    clipRule="evenodd"
+                                  />
+                                  <path d="M2.25 18a.75.75 0 000 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 00-.75-.75H2.25z" />
+                                </svg>
+                              </span>
+                            </Tooltip>
+                          </div>
+                        ))}
+                    </div>
+                  </CardBody>
+                  <CardFooter className="flex flex-col justify-between mt-auto pb-3">
+                    <div className="grid grid-cols-2 gap-2">
+                      <Button
+                        onClick={() => {
+                          Navigate(`/create-summarize/${items.documentId}`);
+                        }}
+                        size="lg"
+                        className="w-full"
+                      >
+                        ดูข้อมูลคอร์ส
+                      </Button>
+                      <Button
+                        onClick={() => {
+                          Navigate("/view-student", {
+                            state: { Value: items },
+                          });
+                        }}
+                        size="lg"
+                        className="w-full"
+                      >
+                        ดูข้อมูลนักเรียน
+                      </Button>
+                    </div>
+                  </CardFooter>
+                </Card>
+              </div>
+            ))}
           </div>
-        ))}
+        ) : (
+          <div className="my-7 flex items-center justify-center">
+            <div className="bg-white shadow-lg border border-gray-200 rounded-xl px-6 py-8 text-center">
+              <p className="text-gray-700 text-2xl font-semibold">
+                ไม่พบข้อมูลคอร์สเรียนที่ค้นหา
+              </p>
+              <p className="text-gray-500 text-lg mt-2">
+                กรุณาตรวจสอบคำค้นหาหรือเพิ่มคอร์สเรียน
+              </p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/src/admin/pages/Finance.js
+++ frontend/src/admin/pages/Finance.js
@@ -65,7 +65,7 @@ const FinanceOrder = () => {
   return (
     <div className="container mx-auto px-4 py-8 max-w-3xl mt-11">
       {/* <div className="w-[1200px] mx-96 mt-11 p-8 ml-32"> */}
-      <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+      <div className="mx-auto text-center mb-8 lg:mb-16">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
           การเงิน
         </h2>
@@ -75,97 +75,125 @@ const FinanceOrder = () => {
 
         <div className="mb-1">
           {/* <h2 className="text-lg font-semibold mb-2">ตัวกรอง:</h2> */}
-          <div className="flex justify-center items-center text-base">
-            {`ค้นหา : `}
+          <div className="flex justify-center items-center space-x-3 bg-white p-4 rounded-lg shadow-md border border-gray-200">
+            <label
+              htmlFor="search"
+              className="text-gray-700 text-lg font-medium"
+            >
+              ค้นหา :
+            </label>
             <input
+              id="search"
               title="ค้นหา"
               type="text"
-              placeholder="ค้นหาโดยชื่อผู้ซื้อ"
-              className="flex-1 bg-white h-10 w-max border-2  border-gray-300 rounded-lg px-2"
+              placeholder="ค้นหาโดยชื่อผู้ชำระเงิน"
+              className="flex-1 bg-gray-100 focus:bg-white h-10 w-72 border border-gray-300 rounded-lg px-4 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
               value={queryPayments}
               onChange={(e) => setQueryPayments(e.target.value)}
             />
-            <button
-              onClick={handleSearch}
-              className="bg-gray-200 text-gray-700 px-4 py-2 rounded-r-3xl hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            <select
+              className="w-48 border border-gray-300 rounded-lg px-4 py-2 text-gray-700 bg-white shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 hover:border-gray-400"
+              value={queryPayments}
+              onChange={(e) => setQueryPayments(e.target.value)}
             >
-              <Search className="h-5 w-5" />
-            </button>
+              <option value="" disabled className="text-gray-500">
+                เลือกสถานะ
+              </option>
+              <option value="Waiting" className="text-gray-700">
+                รอการตรวจสอบ
+              </option>
+              <option value="Confirmed" className="text-gray-700">
+                เสร็จสิ้น
+              </option>
+            </select>
           </div>
         </div>
       </div>
 
       <div className="space-y-4">
-        {filteredPayments.map((payment) => (
-          <div
-            key={payment.id}
-            className={`rounded-xl p-5 shadow-md transition-all duration-300 ${
-              payment.status_confirm == "confirmed"
-                ? "bg-gradient-to-r from-green-100 to-green-50 border-2 border-green-300"
-                : "bg-gradient-to-r from-red-100 to-red-50 border-2 border-red-300"
-            }`}
-          >
-            <div className="flex justify-between items-start">
-              <div>
-                <p className="text-sm text-gray-600">
-                  หมายเลขการสั่งซื้อ : {payment.id}
-                </p>
-                <p className="font-medium">
-                  ผู้ซื้อ : {payment.users_purchase.first_name}{" "}
-                  {payment.users_purchase.last_name}
-                </p>
-                <p className="font-medium">
-                  Email : {payment.users_purchase.email}
-                </p>
-                <p className="text-sm text-gray-600">
-                  เวลาการยืนยันชำระเงิน :{" "}
-                  {new Date(payment.createdAt).toLocaleDateString("th-TH")}{" "}
-                  {new Date(payment.createdAt).toLocaleTimeString("th-TH", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    hour12: false,
-                  })}
-                </p>
-                <p className="text-sm text-gray-600">
-                  คอร์สที่ซื้อ :{" "}
-                  {payment.course_purchase
-                    .map((course) =>
-                      course.Name.length > 30
-                        ? course.Name.slice(0, 30) + "..."
-                        : course.Name
-                    )
-                    .join(", ")}
-                </p>
+        {filteredPayments.length > 0 ? (
+          filteredPayments.map((payment) => (
+            <div
+              key={payment.id}
+              className={`rounded-xl p-5 shadow-md transition-all duration-300 ${
+                payment.status_confirm === "confirmed"
+                  ? "bg-gradient-to-r from-green-100 to-green-50 border-2 border-green-300"
+                  : "bg-gradient-to-r from-red-100 to-red-50 border-2 border-red-300"
+              }`}
+            >
+              <div className="flex justify-between items-start">
+                <div>
+                  <p className="text-sm text-gray-600">
+                    หมายเลขการสั่งซื้อ : {payment.id}
+                  </p>
+                  <p className="font-medium">
+                    ผู้ซื้อ : {payment.users_purchase.first_name}{" "}
+                    {payment.users_purchase.last_name}
+                  </p>
+                  <p className="font-medium">
+                    Email : {payment.users_purchase.email}
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    เวลาการยืนยันชำระเงิน :{" "}
+                    {new Date(payment.createdAt).toLocaleDateString("th-TH")}{" "}
+                    {new Date(payment.createdAt).toLocaleTimeString("th-TH", {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: false,
+                    })}
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    คอร์สที่ซื้อ :{" "}
+                    {payment.course_purchase
+                      .map((course) =>
+                        course.Name.length > 30
+                          ? course.Name.slice(0, 30) + "..."
+                          : course.Name
+                      )
+                      .join(", ")}
+                  </p>
+                </div>
+                {payment.status_confirm === "confirmed" && (
+                  // <div className="bg-green-700 text-white text-2xl px-3 py-1 rounded-full">
+                  //   ยืนยันการชำระเงินแล้ว
+                  // </div>
+                  <img
+                    src={Confirm}
+                    className="px-3 py-1 rounded-full"
+                    alt="Confirm"
+                    style={{ width: "150px" }}
+                  />
+                )}
               </div>
-              {payment.status_confirm == "confirmed" && (
-                // <div className="bg-green-700 text-white text-2xl px-3 py-1 rounded-full">
-                //   ยืนยันการชำระเงินแล้ว
-                // </div>
-                <img
-                  src={Confirm}
-                  className="px-3 py-1 rounded-full"
-                  alt="Confirm"
-                  style={{ width: "150px" }}
-                />
-              )}
+              <div className="flex justify-between items-center mt-4">
+                <p className="text-sm">
+                  จำนวนเงินที่ต้องชำระ : {payment.amount} บาท
+                </p>
+                {payment.status_confirm === "waiting" && (
+                  <button
+                    onClick={() => {
+                      openModal(payment.picture_purchase);
+                    }}
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  >
+                    ตรวจสอบการชำระ
+                  </button>
+                )}
+              </div>
             </div>
-            <div className="flex justify-between items-center mt-4">
-              <p className="text-sm">
-                จำนวนเงินที่ต้องชำระ : {payment.amount} บาท
+          ))
+        ) : (
+          <div className="mx-auto my-7 flex items-center justify-center">
+            <div className="bg-white shadow-lg border border-gray-200 rounded-xl px-6 py-8 text-center">
+              <p className="text-gray-700 text-2xl font-semibold">
+                ไม่พบข้อมูลการเงินที่ค้นหา
               </p>
-              {payment.status_confirm == "waiting" && (
-                <button
-                  onClick={() => {
-                    openModal(payment.picture_purchase);
-                  }}
-                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                >
-                  ตรวจสอบการชำระ
-                </button>
-              )}
+              <p className="text-gray-500 text-lg mt-2">
+                กรุณาตรวจสอบคำค้นหาข้อมูลการเงิน
+              </p>
             </div>
           </div>
-        ))}
+        )}
       </div>
       <Dialog open={open} onClose={setOpen} className="relative z-10">
         <DialogBackdrop
@@ -214,21 +242,21 @@ const FinanceOrder = () => {
                 <button
                   type="button"
                   data-autofocus
-                  onClick={() => setOpen(false)}
+                  onClick={() => closeModal()}
                   className="inline-flex w-max justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-500 sm:ml-3 sm:w-auto"
                 >
                   อนุมัติ
                 </button>
                 <button
                   type="button"
-                  onClick={() => setOpen(false)}
+                  onClick={() => closeModal()}
                   className="inline-flex w-max justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto"
                 >
                   ไม่อนุมัติ
                 </button>
                 <button
                   type="button"
-                  onClick={() => setOpen(false)}
+                  onClick={() => closeModal()}
                   className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto"
                 >
                   ยกเลิก
