40014a072a567bff6de48867116bab05c0e8294c
PunPK <phurutsakorn.kps@gmail.com>
2025-02-12 17:01:51 +0700

    Add: Chartjs for AdminPage and AdminPage

--- frontend/src/admin/pages/home-admin.js
+++ frontend/src/admin/pages/home-admin.js
@@ -1,6 +1,59 @@
 import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../../context/Auth.context";
 // import { BarChart } from "@mui/x-charts/BarChart";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const data = {
+  labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  datasets: [
+    {
+      label: "Income",
+      data: [1200, 1500, 1800, 1700, 1900, 2100, 2300], //จำลองก่อนนะ
+      backgroundColor: "rgba(75, 192, 192, 0.2)",
+      borderColor: "rgba(43, 63, 229, 0.8)",
+      borderWidth: 2,
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  // maintainAspectRatio: false,
+  plugins: {
+    title: {
+      display: true,
+      text: "Daily Income and Expenses",
+    },
+  },
+  scales: {
+    x: {
+      type: "category",
+    },
+    y: {
+      beginAtZero: true,
+    },
+  },
+};
 
 const HomeAdmin = () => {
   const { state: ContextState } = useContext(AuthContext);
@@ -14,7 +67,7 @@ const HomeAdmin = () => {
           <div className="flex-1 bg-indigo-100 border border-indigo-200 rounded-xl p-6 animate-fade-in">
             <h2 className="text-4xl md:text-5xl text-blue-900">
               Welcome to <br />
-              <strong>DashBoard Admin</strong>
+              <strong>DashBoard {user.userRole}</strong>
             </h2>
             <span className="inline-block mt-8 px-8 py-2 rounded-full text-xl font-bold text-white bg-indigo-800">
               คุณ {user.first_name} {user.last_name}
@@ -25,7 +78,7 @@ const HomeAdmin = () => {
           <div className="flex-1 bg-blue-100 border border-blue-200 rounded-xl p-6 animate-fade-in">
             <h2 className="text-3xl md:text-4xl text-blue-900">
               ตรวจสอบสถานะการจ่ายเงิน <br />
-              <strong>รอตรวจสอบ : 23</strong>
+              <strong>รอตรวจสอบ : 1</strong>
             </h2>
             <a
               href="/finance"
@@ -45,8 +98,9 @@ const HomeAdmin = () => {
             <h3 className="text-xl font-bold text-indigo-800">
               จำนวนคอร์สทั้งหมดของ TechSphere
             </h3>
-            <p className="text-9xl md:text-7xl text-blue-900 mt-4 flex ">
-              <strong>23 คอร์ส</strong>
+            <p className="text-9xl md:text-7xl text-blue-900 mt-4 flex justify-center">
+              <strong className="text-9xl">23</strong>
+              <h3 className="flex items-end">คอร์ส</h3>
             </p>
           </div>
           <div
@@ -54,10 +108,11 @@ const HomeAdmin = () => {
             style={{ animationDelay: "0.2s" }}
           >
             <h3 className="text-xl font-bold text-indigo-800">
-              นักเรียนทั้งหมด
+              นักเรียนทั้งหมดของ TechSphere
             </h3>
-            <p className="text-9xl md:text-7xl text-blue-900 mt-4 flex ">
-              <strong>3 คน</strong>
+            <p className="text-9xl md:text-7xl text-blue-900 mt-4 flex justify-center">
+              <strong className="text-9xl">2</strong>
+              <h3 className="flex items-end">คน</h3>
             </p>
           </div>
           <div
@@ -65,25 +120,24 @@ const HomeAdmin = () => {
             style={{ animationDelay: "0.3s" }}
           >
             <h3 className="text-xl font-bold text-indigo-800">
-              อาจารย์ผู้สอนทั้งหมด
+              อาจารย์ผู้สอนทั้งหมดของ TechSphere
             </h3>
-            <p className="text-9xl md:text-7xl text-blue-900 mt-4 flex ">
-              <strong>2 คน</strong>
+            <p className="text-9xl md:text-7xl text-blue-900 mt-4 flex justify-center">
+              <strong className="text-9xl">3</strong>
+              <h3 className="flex items-end">คน</h3>
             </p>
           </div>
         </div>
 
         <div className="flex-1 bg-indigo-100 border border-indigo-200 rounded-xl p-6 animate-fade-in mt-5">
           <div
-            className="bg-white rounded-xl shadow-lg p-6 h-64 transition-all duration-300 hover:-translate-y-1 hover:shadow-xl animate-slide-up"
+            className="bg-white rounded-xl shadow-lg p-6 h-max transition-all duration-300 hover:-translate-y-1 hover:shadow-xl animate-slide-up"
             style={{ animationDelay: "0.1s" }}
           >
-            <h3 className="text-xl font-bold text-indigo-800">
-              จำนวนคอร์สทั้งหมดของ TechSphere
-            </h3>
-            <p className="text-9xl md:text-7xl text-blue-900 mt-4 flex ">
-              <strong>23 คอร์ส</strong>
-            </p>
+            <div className="dataCard revenueCard">
+              {/* <div className="w-full max-w-2xl h-[400px] sm:h-[300px] mx-auto bg-white p-4 rounded-2xl shadow-lg"> */}
+              <Line data={data} options={options} />
+            </div>
           </div>
         </div>
       </main>
