75bd02491c031ac6c184cef76bc720080558c5e7
PunPK <phurutsakorn.kps@gmail.com>
2025-03-01 20:57:35 +0700

    Add: env for deployment

--- frontend/src/conf/main.js
+++ frontend/src/conf/main.js
@@ -1,9 +1,14 @@
+const apiUrl =
+  process.env.REACT_APP_STATUS === "production"
+    ? process.env.REACT_APP_PRODUCTION_URL
+    : process.env.REACT_APP_DEVELOP_URL;
+
 const conf = {
-  apiUrlPrefix: "http://localhost:1337/api",
+  apiUrlPrefix: `${apiUrl}/api`,
   loginEndpoint: "/auth/local",
   jwtUserEndpoint: "/users/me",
   jwtSessionStorageKey: "auth.jwt",
-  apiUrl: "http://localhost:1337",
+  apiUrl,
 };
 
 export default conf;
