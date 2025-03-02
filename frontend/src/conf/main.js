7c2ad58594a740b8781eabf67aac97f9d2022921
PunPK <phurutsakorn.kps@gmail.com>
2025-03-02 20:51:10 +0700

    add file for prod

--- frontend/src/conf/main.js
+++ frontend/src/conf/main.js
@@ -1,14 +1,25 @@
-const apiUrl =
-  process.env.REACT_APP_STATUS === "production"
-    ? process.env.REACT_APP_PRODUCTION_URL
-    : process.env.REACT_APP_DEVELOP_URL;
+const isProd = process.env.NODE_ENV === "production";
 
-const conf = {
-  apiUrlPrefix: `${apiUrl}/api`,
-  loginEndpoint: "/auth/local",
-  jwtUserEndpoint: "/users/me",
-  jwtSessionStorageKey: "auth.jwt",
-  apiUrl,
+const config = {
+  isProd,
+  apiUrlPrefix: isProd
+    ? "https://w11-admin.pupasoft.com/api"
+    : "http://localhost:1337/api",
 };
 
-export default conf;
+export default config;
+
+// const apiUrl =
+//   process.env.REACT_APP_STATUS === "production"
+//     ? process.env.REACT_APP_PRODUCTION_URL
+//     : process.env.REACT_APP_DEVELOP_URL;
+
+// const conf = {
+//   apiUrlPrefix: `${apiUrl}/api`,
+//   loginEndpoint: "/auth/local",
+//   jwtUserEndpoint: "/users/me",
+//   jwtSessionStorageKey: "auth.jwt",
+//   apiUrl,
+// };
+
+// export default conf;
