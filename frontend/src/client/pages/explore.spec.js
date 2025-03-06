aba1f79f275f2c0bde830d04cb090a98727a1a46
6710110151 <6710110151@psu.ac.th>
2025-03-06 21:31:37 +0700

    test playwright expolre page 6710110151

--- frontend/src/client/pages/explore.spec.js
+++ frontend/src/client/pages/explore.spec.js
@@ -10,32 +10,32 @@ test.describe("Explore Page - Filters", () => {
   });
 
   test("Should filter courses by search query", async ({ page }) => {
-    await page.fill('input[placeholder="Search courses..."]', "React");
-    await page.waitForTimeout(1000);
-    const courseCount = await page.locator(".grid > div").count();
-    expect(courseCount).toBeGreaterThan(0);
-  });
-
-  test("Should filter courses by category", async ({ page }) => {
-    await page.click("text=หมวดหมู่คอร์ส");
-    await page.click("text=Web Develop");
-    await page.waitForTimeout(1000);
-    const courseCount = await page.locator(".grid > div").count();
-    expect(courseCount).toBeGreaterThan(0);
-  });
-
-  test("Should filter courses by rating", async ({ page }) => {
-    await page.click('input[id="rating-4"]');
-    await page.waitForTimeout(1000);
-    const courseCount = await page.locator(".grid > div").count();
+    await page.fill('input[placeholder="Search courses..."]', "Javascript");
+    await page.waitForSelector('.grid > div:has-text("Javascript")');
+    const courseCount = await page
+      .locator('.grid > div:has-text("Javascript")')
+      .count();
     expect(courseCount).toBeGreaterThan(0);
   });
 
   test("Should reset filters", async ({ page }) => {
     await page.click("text=รีเซ็ตค่าที่ใช้");
-    await page.waitForTimeout(1000);
-
+    await page.waitForTimeout(2000);
     const courseCount = await page.locator(".grid > div").count();
     expect(courseCount).toBeGreaterThan(0);
+  });
+
+  test("Should display no courses found message", async ({ page }) => {
+    await page.fill(
+      'input[placeholder="Search courses..."]',
+      "Nonexistent Course"
+    );
+    await page.waitForSelector("text=❌ ไม่พบคอร์สที่ต้องการ");
+    await expect(page.locator("text=❌ ไม่พบคอร์สที่ต้องการ")).toBeVisible();
+  });
+
+  test("Should navigate to viewCourse page", async ({ page }) => {
+    await page.click(".grid > div");
+    await expect(page.url()).toContain("/view-product/");
   });
 });
